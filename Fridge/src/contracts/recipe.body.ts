22cc6e9a3f54954000da5f49e4abf8fbd05fcc9e
Daan Schepers <schepers.daan@gmail.com>
2025-07-14 17:07:56 +0200

    fixed creating recipes

--- Fridge/src/contracts/recipe.body.ts
+++ Fridge/src/contracts/recipe.body.ts
@@ -1,16 +1,9 @@
 import { Exclude, Expose } from "class-transformer";
-import { IsEmail, IsNumber, isString, IsString, Length } from "class-validator";
+import { IsArray, IsEmail, IsNumber, isString, IsString, Length } from "class-validator";
 import { ProductBody } from "./product.body";
 import { ApiProperty } from "@nestjs/swagger";
-
-// For safety we'll exclude everything from being transformed by placing a @Exclude() decorator on the class declaration
 @Exclude()
 class RecipeBody {
-	// We can expose the properties we want included one by one
-    // @Expose()
-    // @IsNumber()
-    // public id: string;
-    
 	@Expose()
 	@IsString()
     @ApiProperty()
@@ -27,8 +20,10 @@ class RecipeBody {
     public owner: string;
 
     @Expose()
-    @ApiProperty()
-    public ingredients: ProductBody[];
+    @IsArray()
+    @IsString({each: true})
+@ApiProperty({type: [String]})
+    public ingredients: string[];
 }
 
 export { RecipeBody };
